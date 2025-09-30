a61d8a1c5543761a74f2426bd33b93033410147e
Patresia-A <paguil12@msudenver.edu>
2025-09-30 16:39:38 -0600

    Create spotify.ts

---
+++ client/src/sdk/spotify.ts
@@ -0,0 +1,26 @@
+let sdkReadyResolve: () => void
+export const spotifySDKReady = new Promise<void>(res => (sdkReadyResolve = res))
+
+// @ts-ignore
+window.onSpotifyWebPlaybackSDKReady = () => {
+  sdkReadyResolve()
+}
+
+export async function getSpotifyAccessToken(): Promise<string> {
+  const r = await fetch('/api/spotify/access_token')
+  if (!r.ok) throw new Error('Not authenticated with Spotify')
+  const j = await r.json()
+  return j.access_token
+}
+
+export async function createSpotifyPlayer() {
+  await spotifySDKReady
+  const token = await getSpotifyAccessToken()
+  // @ts-ignore
+  const player = new window.Spotify.Player({
+    name: 'Rhythm Web Player',
+    getOAuthToken: (cb: (t: string) => void) => cb(token)
+  })
+  await player.connect()
+  return player
+}
