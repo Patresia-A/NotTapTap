80fe60e87a08dc9a4580d0ec8f23d9d9bfc8a883
Patresia-A <paguil12@msudenver.edu>
2025-09-30 16:38:20 -0600

    Create apple.ts

---
+++ client/src/sdk/apple.ts
@@ -0,0 +1,11 @@
+export async function initMusicKit(): Promise<MusicKit.MusicKitInstance> {
+  // @ts-ignore
+  const MusicKit = window.MusicKit as typeof window.MusicKit
+  const devToken = await fetch('/api/apple/devtoken').then(r => r.text())
+  const music = await MusicKit.configure({
+    developerToken: devToken,
+    app: { name: 'Rhythm Starter', build: '1.0.0' }
+  })
+  await music.authorize()
+  return music
+}
