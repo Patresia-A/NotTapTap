96f3097322293987243622a50dc0991037c8bf64
Patresia-A <paguil12@msudenver.edu>
2025-10-09 14:54:13 -0600

    Update apple.ts

--- client/src/sdk/apple.ts
+++ client/src/sdk/apple.ts
@@ -1,7 +1,9 @@
+import { apiUrl } from '../config'
+
 export async function initMusicKit(): Promise<MusicKit.MusicKitInstance> {
   // @ts-ignore
   const MusicKit = window.MusicKit as typeof window.MusicKit
-  const devToken = await fetch('/api/apple/devtoken').then(r => r.text())
+  const devToken = await fetch(apiUrl('/api/apple/devtoken')).then(r => r.text())
   const music = await MusicKit.configure({
     developerToken: devToken,
     app: { name: 'Rhythm Starter', build: '1.0.0' }
